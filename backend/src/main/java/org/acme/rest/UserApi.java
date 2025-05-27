5a06dfe70930415db15be7eaf26b0128a0cbd0dc
jhainethv161 <jhainethv.naranjom@uqvirtual.edu.co>
2025-05-26 22:42:31 -0500

    Obtencion de todos los usuarios

--- backend/src/main/java/org/acme/rest/UserApi.java
+++ backend/src/main/java/org/acme/rest/UserApi.java
@@ -6,9 +6,12 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.acme.model.user.User;
 import org.acme.model.user.UserAuth;
+import org.acme.model.user.UserDTO;
 import org.acme.service.UserService;
 import org.acme.utils.mappers.MapToList;
 import org.acme.utils.tree.BinaryTree;
+
+import java.util.List;
 
 @Path("/user")
 public class UserApi {
@@ -37,6 +40,20 @@ public class UserApi {
             User user = userService.userAuth(userAuth.getUser(), userAuth.getPassword());
             if (user != null) return Response.status(Response.Status.OK).entity(MapToList.userToUserDTO(user)).build();
             return Response.status(Response.Status.UNAUTHORIZED).build();
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Ha ocurrido un error al intentar " +
+                    "procesar la solicitud. Por favor intente más tade.").build();
+        }
+    }
+
+    @GET
+    @Path("/get-all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getUsers(){
+        try {
+            BinaryTree<User> users = userService.getUsers();
+            List<UserDTO> usersDTO = MapToList.binaryTreeUserToList(users);
+            return Response.status(Response.Status.OK).entity(usersDTO).build();
         } catch (Exception e) {
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Ha ocurrido un error al intentar " +
                     "procesar la solicitud. Por favor intente más tade.").build();
